b264c0f85af5ab69392882b1deb8907110fd90bb
Matthew Davis <makaio.hiwalani@gmail.com>
2017-02-09 19:17:26 +0000

    build:commit

--- src/modules/platform-server-sdk/platform-server-sdk.module.ts
+++ src/modules/platform-server-sdk/platform-server-sdk.module.ts
@@ -6,7 +6,7 @@ import { Observer }                         from "rxjs/Observer";
 import { Observable }                       from "rxjs/Observable";
 import { BehaviorSubject }                  from "rxjs/Rx";
 import { PlatformServerSDKConfig }          from './platform-server-sdk.config';
-import { PlatformServerSDKService }         from './platform-server-sdk.service';
+// import { PlatformServerSDKService }         from './platform-server-sdk.service';
 import { PlatformServerSDKRoutingModule }   from './platform-server-sdk-routing.module';
 
 @NgModule({
